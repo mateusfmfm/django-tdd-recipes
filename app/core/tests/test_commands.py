29c6871d12640f3bad9077222420a8cd8c417865
Mateus Félix <mateusfmfm@outlook.com>
2023-08-01 16:48:48 -0300

    lints

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,19 +11,18 @@ from psycopg2 import OperationalError as Psycopg2OpError
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
 
-    """Test waiting for DB if DB is ready"""
     def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for DB if DB is ready"""
         patched_check.return_value = True
         call_command('wait_for_db')
         patched_check.assert_called_once_with(databases=['default'])
 
-
-    """Test waiting for DB when getting OperationalError"""
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        patched_check.side_effect = [Psycopg2OpError] * 2 \
-        + [OperationalError] * 3 + [True]
-        
+        """Test waiting for DB when getting OperationalError"""
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
+            [OperationalError] * 3 + [True]
+
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
