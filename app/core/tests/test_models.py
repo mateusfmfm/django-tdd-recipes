287fb81b2466ec7d44975f217a723adefd5e76e0
Mateus Félix <mateusfmfm@outlook.com>
2023-10-13 00:19:26 -0300

    tags flow

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,6 +5,11 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 
+def create_user(email="user@example.com", password="testpass123"):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email = "test@example.com"
@@ -54,3 +59,9 @@ class ModelTests(TestCase):
             description="Sample recipe description.",
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name="Tag1")
+        self.assertEqual(str(tag), tag.name)
