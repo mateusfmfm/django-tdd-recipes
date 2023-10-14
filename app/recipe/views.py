e6339be4a91da8594a5cc0ee97b0ad517d67b2bc
Mateus Félix <mateusfmfm@outlook.com>
2023-10-14 14:43:12 -0300

    removing lints

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -20,7 +20,8 @@ from rest_framework.response import Response
             OpenApiParameter(
                 "ingredients",
                 OpenApiTypes.STR,
-                description="Comma separated list of ingredients IDs to filter",
+                description="Comma separated list of ingredients IDs \
+                    to filter",
             ),
         ]
     )
@@ -49,7 +50,13 @@ class RecipeViewSet(viewsets.ModelViewSet):
             ingredient_id = self._params_to_ints(ingredients)
             queryset = queryset.filter(ingredients__id__in=ingredient_id)
 
-        return queryset.filter(user=self.request.user).order_by("-id").distinct()
+        return (
+            queryset.filter(
+                user=self.request.user,
+            )
+            .order_by("-id")
+            .distinct()
+        )
 
     def get_serializer_class(self):
         """Return the serializer class for request"""
@@ -102,12 +109,23 @@ class BaseRecipeAttrViewSet(
     def get_queryset(self):
         """Filter queryset to authenticated user"""
         assigned_only = bool(
-            int(self.request.query_params.get("assigned_only", 0)),
+            int(
+                self.request.query_params.get(
+                    "assigned_only",
+                    0,
+                )
+            ),
         )
         queryset = self.queryset
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
-        return queryset.filter(user=self.request.user).order_by("-name").distinct()
+        return (
+            queryset.filter(
+                user=self.request.user,
+            )
+            .order_by("-name")
+            .distinct()
+        )
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
