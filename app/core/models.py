83978ee80d1c7782a73954ead333fc149e80fd56
Mateus Félix <mateusfmfm@outlook.com>
2023-10-13 22:55:26 -0300

    ingredients flow

--- app/core/models.py
+++ app/core/models.py
@@ -45,6 +45,7 @@ class Recipe(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=5, blank=True)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField("Tag")
+    ingredients = models.ManyToManyField("Ingredient")
 
     def __str__(self):
         return self.title
@@ -52,6 +53,19 @@ class Recipe(models.Model):
 
 class Tag(models.Model):
     """Tag for filtering recipes."""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient for recipes"""
 
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
